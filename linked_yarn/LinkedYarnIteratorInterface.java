efa9e64c633fb172351d23323cec23b1d051a058
BlakeCrowther7 <blakecrowther7@gmail.com>
2018-04-15 22:01:40 -0700

    Create LinkedYarnIteratorInterface.java

---
+++ linked_yarn/LinkedYarnIteratorInterface.java
@@ -0,0 +1,11 @@
+package linked_yarn;
+
+public interface LinkedYarnIteratorInterface {
+    boolean isValid ();
+    boolean hasNext ();
+    boolean hasPrev ();
+    String getString ();
+    void next ();
+    void prev ();
+    void replaceAll (String toReplaceWith);
+}
